a1c13ceaa8b6d36694caabc56e779af6ed696c0e
behera-abhisar <behera.abhisar@gmail.com>
2025-07-16 01:04:08 +0530

    Initial commit

---
+++ src.cpp
@@ -0,0 +1,18 @@
+// Name: Abhisar Behera
+// PRN: 24070123002
+// Experiment - 1
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num_1, num_2, sum_out;
+
+    cout << "Enter the first number: ";
+    cin >> num_1;
+    cout << "Enter the second number: ";
+    cin >> num_2;
+    sum_out = num_1 + num_2;
+    cout << "Sum of " << num_1 << " and " << num_2 << " is " << sum_out << endl;
+}
