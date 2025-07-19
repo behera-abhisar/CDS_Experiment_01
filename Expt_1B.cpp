41352f8a8dc39981b910ed1f9705acdaf0828f26
Abhisar Behera <behera.abhisar@gmail.com>
2025-07-20 02:48:59 +0530

    Update Expt_1B.cpp

--- Expt_1B.cpp
+++ Expt_1B.cpp
@@ -6,14 +6,23 @@
 
 using namespace std;
 
-int main() {
-    int num_1, num_2, sum_out;
+int main()
+{
+    int num_1, num_2, sum_out, sub_out, mul_out, div_out;
 
     cout << "Enter the first number: ";
     cin >> num_1;
     cout << "Enter the second number: ";
     cin >> num_2;
+
     sum_out = num_1 + num_2;
+    sub_out = num_1 - num_2;
+    mul_out = num_1 * num_2;
+    div_out = num_1 / num_2;
+
     cout << "Sum of " << num_1 << " and " << num_2 << " is " << sum_out << endl;
+    cout << "Subtraction of " << num_1 << " from " << num_2 << " is " << sub_out << endl;
+    cout << "Multiplication of " << num_1 << " and " << num_2 << " is " << mul_out << endl;
+    cout << "Division of " << num_1 << " and " << num_2 << " is " << div_out << endl;
     return 0;
 }
